8906e2344c1307d8e1b43e1e0b8fc80dd4532974
Brad <bcoffield@gmail.com>
2020-04-22 11:37:27 -0600

    styling add cached search

--- src/views/AddCachedSearch.vue
+++ src/views/AddCachedSearch.vue
@@ -1,190 +1,134 @@
 <template>
   <div class="container">
     <h2 class="title is-2">Add Cached Search</h2>
-    <b-field label="Cache a new search - EBSCO"> </b-field>
-    <div class="block">
-      <b-field label="Search term(s)">
-        <b-input v-model="cacheNewSearch_Ebsco.value"></b-input>
-      </b-field>
-
-      <b-checkbox v-model="cacheNewSearch_Ebsco.fulltext">
-        Full-text only
-      </b-checkbox>
-
-      <b-checkbox v-model="cacheNewSearch_Ebsco.daterange">
-        Recent only
-      </b-checkbox>
-
-      <b-checkbox v-model="cacheNewSearch_Ebsco.scholarly">
-        Scholarly only
-      </b-checkbox>
-    </div>
-    <!-- <b-button @click="">Click Me</b-button> -->
-    <b-button
-      @click="cacheSearch('ebsco-search', cacheNewSearch_Ebsco)"
-      type="is-primary"
-      outlined
-      >Cache New Search</b-button
-    >
-
     <hr />
 
-    <b-field label="Cache a new search - Primo Books"> </b-field>
-    <div class="block">
-      <b-field label="Search term(s)">
-        <b-input v-model="cacheNewSearch_PrimoBooks.value"></b-input>
-      </b-field>
-
+    <h3 class="title is-3" style="margin-top:4rem">EBSCO</h3>
+    <b-field label="Search term(s)">
+      <b-input v-model="cacheNewSearch_Ebsco.value"></b-input>
+    </b-field>
+    <div class="field">
+      <b-field label="Options:"></b-field>
       <div class="block">
-        Precision:
+        <b-checkbox v-model="cacheNewSearch_Ebsco.fulltext">Full-text only</b-checkbox>
 
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.precision"
-          name="name1"
-          native-value="exact"
-        >
-          Exact
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.precision"
-          name="name1"
-          native-value="begins_with"
-        >
-          Begins with
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.precision"
-          name="name1"
-          native-value="contains"
-        >
-          Contains
-        </b-radio>
+        <b-checkbox v-model="cacheNewSearch_Ebsco.daterange">Recent only</b-checkbox>
+
+        <b-checkbox v-model="cacheNewSearch_Ebsco.scholarly">Scholarly only</b-checkbox>
       </div>
-
-      <!-- ********************* -->
-
-      <div class="block">
-        Field:
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.field"
-          name="name"
-          native-value="any"
-        >
-          Any
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.field"
-          name="name"
-          native-value="title"
-        >
-          Title
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.field"
-          name="name"
-          native-value="creator"
-        >
-          Creator (author)
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoBooks.field"
-          name="name"
-          native-value="sub"
-        >
-          Subject
-        </b-radio>
-      </div>
-    </div>
-    <!-- <b-button @click="">Click Me</b-button> -->
-    <b-button
-      @click="cacheSearch('primo-book-search', cacheNewSearch_PrimoBooks)"
-      type="is-primary"
-      outlined
-      >Cache New Search</b-button
-    >
-
-    <hr />
-
-    <b-field label="Cache a new search - Primo Articles"> </b-field>
-    <div class="block">
-      <b-field label="Search term(s)">
-        <b-input v-model="cacheNewSearch_PrimoArticles.value"></b-input>
-      </b-field>
-
-      <div class="block">
-        Precision:
-
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.precision"
-          name="name1"
-          native-value="exact"
-        >
-          Exact
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.precision"
-          name="name1"
-          native-value="begins_with"
-        >
-          Begins with
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.precision"
-          name="name1"
-          native-value="contains"
-        >
-          Contains
-        </b-radio>
-      </div>
-
-      <!-- ********************* -->
-
-      <div class="block">
-        Field:
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.field"
-          name="name"
-          native-value="any"
-        >
-          Any
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.field"
-          name="name"
-          native-value="title"
-        >
-          Title
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.field"
-          name="name"
-          native-value="creator"
-        >
-          Creator (author)
-        </b-radio>
-        <b-radio
-          v-model="cacheNewSearch_PrimoArticles.field"
-          name="name"
-          native-value="sub"
-        >
-          Subject
-        </b-radio>
-      </div>
-      <div class="block">
-        <b-checkbox v-model="cacheNewSearch_PrimoArticles.fulltext">
-          Full-text only
-        </b-checkbox>
-      </div>
-      <!-- <b-button @click="">Click Me</b-button> -->
       <b-button
-        @click="
-          cacheSearch('primo-article-search', cacheNewSearch_PrimoArticles)
-        "
+        @click="cacheSearch('ebsco-search', cacheNewSearch_Ebsco)"
         type="is-primary"
         outlined
-        >Cache New Search</b-button
-      >
+        class="is-pulled-right"
+      >Cache New Search</b-button>
     </div>
+
+    <!-- <b-field label></b-field> -->
+
+    <!-- <b-button @click="">Click Me</b-button> -->
+
+    <div style="margin-bottom:4rem;">&nbsp;</div>
+
+    <h3 class="title is-3" style="margin-top:4rem">Primo Books</h3>
+
+    <b-field label="Search term(s)">
+      <b-input v-model="cacheNewSearch_PrimoBooks.value"></b-input>
+    </b-field>
+
+    <div class="field">
+      <b-field label="Precision:"></b-field>
+      <div class="block" style="margin-left:1.5rem;">
+        <b-radio
+          v-model="cacheNewSearch_PrimoBooks.precision"
+          name="name1"
+          native-value="exact"
+        >Exact</b-radio>
+        <b-radio
+          v-model="cacheNewSearch_PrimoBooks.precision"
+          name="name1"
+          native-value="begins_with"
+        >Begins with</b-radio>
+        <b-radio
+          v-model="cacheNewSearch_PrimoBooks.precision"
+          name="name1"
+          native-value="contains"
+        >Contains</b-radio>
+      </div>
+
+      <!-- ********************* -->
+
+      <b-field label="Field:"></b-field>
+      <div class="block" style="margin-left:1.5rem">
+        <b-radio v-model="cacheNewSearch_PrimoBooks.field" name="name" native-value="any">Any</b-radio>
+        <b-radio v-model="cacheNewSearch_PrimoBooks.field" name="name" native-value="title">Title</b-radio>
+        <b-radio
+          v-model="cacheNewSearch_PrimoBooks.field"
+          name="name"
+          native-value="creator"
+        >Creator (author)</b-radio>
+        <b-radio v-model="cacheNewSearch_PrimoBooks.field" name="name" native-value="sub">Subject</b-radio>
+      </div>
+
+      <b-button
+        @click="cacheSearch('primo-book-search', cacheNewSearch_PrimoBooks)"
+        type="is-primary"
+        outlined
+        class="is-pulled-right"
+      >Cache New Search</b-button>
+    </div>
+
+    <div style="margin-bottom:4rem;">&nbsp;</div>
+    <h3 class="title is-3" style="margin-top:4rem">Primo Articles</h3>
+    <b-field label="Search term(s)">
+      <b-input v-model="cacheNewSearch_PrimoArticles.value"></b-input>
+    </b-field>
+    <b-field label="Precision:"></b-field>
+    <div class="block">
+      <b-radio
+        v-model="cacheNewSearch_PrimoArticles.precision"
+        name="name1"
+        native-value="exact"
+      >Exact</b-radio>
+      <b-radio
+        v-model="cacheNewSearch_PrimoArticles.precision"
+        name="name1"
+        native-value="begins_with"
+      >Begins with</b-radio>
+      <b-radio
+        v-model="cacheNewSearch_PrimoArticles.precision"
+        name="name1"
+        native-value="contains"
+      >Contains</b-radio>
+    </div>
+
+    <!-- ********************* -->
+
+    <b-field label="Field:"></b-field>
+    <div class="block">
+      <b-radio v-model="cacheNewSearch_PrimoArticles.field" name="name" native-value="any">Any</b-radio>
+      <b-radio v-model="cacheNewSearch_PrimoArticles.field" name="name" native-value="title">Title</b-radio>
+      <b-radio
+        v-model="cacheNewSearch_PrimoArticles.field"
+        name="name"
+        native-value="creator"
+      >Creator (author)</b-radio>
+      <b-radio v-model="cacheNewSearch_PrimoArticles.field" name="name" native-value="sub">Subject</b-radio>
+    </div>
+    <b-field label="Full-text:"></b-field>
+    <div class="block">
+      <b-checkbox v-model="cacheNewSearch_PrimoArticles.fulltext">Full-text only</b-checkbox>
+    </div>
+    <!-- <b-button @click="">Click Me</b-button> -->
+    <b-button
+      @click="
+          cacheSearch('primo-article-search', cacheNewSearch_PrimoArticles)
+        "
+      type="is-primary"
+      outlined
+      class="is-pulled-right"
+    >Cache New Search</b-button>
+    <p>.</p>
   </div>
 </template>
 <script>
@@ -216,7 +160,7 @@ export default {
     };
   },
   created() {
-    this.successNotifcation("YES");
+    // this.successNotifcation("YES");
   },
   methods: {
     successNotifcation(message) {
@@ -264,3 +208,14 @@ export default {
   }
 };
 </script>
+<style scoped>
+.block {
+  margin-left: 1.5rem;
+}
+.field {
+  padding: 0.25rem 1rem;
+}
+.mbEh {
+  margin-bottom: 3rem;
+}
+</style>
